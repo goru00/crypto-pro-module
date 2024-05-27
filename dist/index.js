816833f4811ce8d1cade8adcb584f0c9cea10adf
goru00 <goru00@vk.com>
2024-05-27 10:18:35 +0300

    проверка на существование window объекта

--- dist/index.js
+++ dist/index.js
@@ -10,31 +10,19 @@ var _CryptoProAPI2 = _interopRequireDefault(_CryptoProAPI);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// подключаем API КриптоПРО
-require('./lib/cadesplugin_api.js');
-
 /** Инициализация работы ЭЦП КриптоПро */
-const init = () => {
-  return new Promise((resolve, reject) => {
-    if (window.cadesplugin !== undefined) {
-      undefined.cadesplugin = window.cadesplugin;
-      resolve(_CryptoProAPI2.default);
+const init = async () => {
+  try {
+    if (window.cadesplugin === undefined) {
+      // подключаем API КриптоПРО
+      require('./lib/cadesplugin_api.js');
+      await window.cadesplugin;
     }
 
-    const scriptCryptoProWeb = document.createElement('script');
-    scriptCryptoProWeb.id = 'crypto-pro';
-    scriptCryptoProWeb.src = './lib/cadesplugin_api.js';
-    document.body.appendChild(scriptCryptoProWeb);
-
-    scriptCryptoProWeb.onerror = e => {
-      reject(e);
-    };
-
-    scriptCryptoProWeb.onload = () => {
-      undefined.cadesplugin = window.cadesplugin;
-      resolve(_CryptoProAPI2.default);
-    };
-  });
+    return _CryptoProAPI2.default;
+  } catch (err) {
+    throw new Error(err);
+  }
 };
 
 exports.default = init;
